f4631539ecc0e44e3dce91b631e8ee70e298fb78
Adam Soltys <asoltys@gmail.com>
2024-09-08 22:55:51 -0700

    handle named args

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let method = "POST";
 let str = (p, params) => JSON.stringify({ method: p.toLowerCase(), params });
+let f = (a) => (a.length === 1 && typeof a[0] === "object" ? a[0] : a);
 
 export default ({ host, port, wallet, user, password }) => {
   let u = `http://${host}:${port}/wallet/${wallet}`;
   let token = btoa(`${user}:${password}`);
   let headers = { authorization: `Basic ${token}` };
-  let post = (body) => fetch(u, { method, headers, body }).then((r) => r.json());
-  let get = (_, prop) => (...params) => post(str(prop, params)).then((r) => r.result);
-
+  let p = (body) => fetch(u, { method, headers, body }).then((r) => r.json());
+  let get = (_, prop) => (...args) => p(str(prop, f(args))).then((r) => r.result);
   return new Proxy({}, { get });
 };
