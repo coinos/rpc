c69ee4f38dd0f5e2bfa249cba7dec9d266eb4069
Adam Soltys <asoltys@gmail.com>
2024-09-09 16:26:08 -0700

    throw errors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 let method = "POST";
 let str = (p, params) => JSON.stringify({ method: p.toLowerCase(), params });
-let f = (a) => (a.length === 1 && typeof a[0] === "object" ? a[0] : a);
+let f = (a) => (a.length === 1 && typeof a[0] === "object" && !Array.isArray(a[0]) ? a[0] : a);
+let h = (r) => { if (r.error) throw new Error(r.error); return r.result; };
 
 export default ({ host, port, wallet, user, password }) => {
   let u = `http://${host}:${port}/wallet/${wallet}`;
   let token = btoa(`${user}:${password}`);
   let headers = { authorization: `Basic ${token}` };
   let p = (body) => fetch(u, { method, headers, body }).then((r) => r.json());
-  let get = (_, prop) => (...args) => p(str(prop, f(args))).then((r) => r.result);
+  let get = (_, prop) => (...args) =>  p(str(prop, f(args))).then(h);
   return new Proxy({}, { get });
 };
