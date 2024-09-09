3472127605e5deae8870dd900351d7ea0b48102e
Adam Soltys <asoltys@gmail.com>
2024-09-08 22:31:09 -0700

    0.0.2

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 let method = "POST";
-let s = (p, params) => JSON.stringify({ method: p.toLowerCase(), params });
+let str = (p, params) => JSON.stringify({ method: p.toLowerCase(), params });
 
 export default ({ host, port, wallet, user, password }) => {
   let u = `http://${host}:${port}/wallet/${wallet}`;
   let token = btoa(`${user}:${password}`);
   let headers = { authorization: `Basic ${token}` };
-  let p = (body) => fetch(u, { method, headers, body }).then((r) => r.json());
-
-  let get =
-    (_, prop) =>
-    (...params) =>
-      p(s(prop, params)).then((r) => r.result);
+  let post = (body) => fetch(u, { method, headers, body }).then((r) => r.json());
+  let get = (_, prop) => (...params) => post(str(prop, params)).then((r) => r.result);
 
   return new Proxy({}, { get });
 };
