9bae6596f9ffedd995c410ed5ef088a8ad2c01c3
Adam Soltys <asoltys@gmail.com>
2024-09-12 08:55:01 -0700

    throw error properly

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let method = "POST";
 let str = (p, params) => JSON.stringify({ method: p.toLowerCase(), params });
 let f = (a) => (a.length === 1 && typeof a[0] === "object" && !Array.isArray(a[0]) ? a[0] : a);
-let h = (r) => { if (!r) return; if (r.error) throw new Error(r.error); return r.result; };
+let h = (r) => { if (!r) return; if (r.error) throw r.error; return r.result; };
 
 export default ({ host, port, wallet, user, password }) => {
   let u = `http://${host}:${port}/wallet/${wallet}`;
